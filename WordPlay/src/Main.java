b19fea066db3e0b1ef434ed23f1f7bd0c8c58998
mhesham <mhesham>
2020-05-31 23:15:50 +0200

    Add Word play project

---
+++ WordPlay/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        WordPlay wordPlay = new WordPlay();
+        wordPlay.testIsVowel();
+        wordPlay.testReplaceVowels();
+        wordPlay.testEmphasize();
+    }
+}
